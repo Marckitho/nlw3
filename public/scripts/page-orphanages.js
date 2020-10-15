dfb002e299af62fd569bbe5b80f3e3df34bb2134
Marco André Babinski <marcoandrebabinski@yahoo.com.br>
2020-10-15 17:56:48 -0300

    dia4
    Alterações do Dia #4 da Next Level Week 3 - Trilha Discovery

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,27 +1,41 @@
-const map = L.map('mapid').setView([-26.9886589,-53.5335547], 16);
+const map = L.map("mapid").setView([-26.9886589, -53.5335547], 16);
 
 // Create and add TileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // Create Icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
-// Create Popup Overlay
-const popup = L.popup({
+function addMarker({id, name, lat, lng}) {
+  // Create Popup Overlay
+  const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`
+  );
 
-// Create and add Marker
-L
-.marker([-26.9886589,-53.5335547], { icon })
-.addTo(map)
-.bindPopup(popup)
+  // Create and add Marker
+  L.marker([lat, lng], { icon })
+  .addTo(map)
+  .bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
---
+++ src/server.js
@@ -0,0 +1,23 @@
+// Importar dependência
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+// Iniciando o express
+const server = express();
+server
+// Utilizando os arquivos estáticos
+.use(express.static('public'))
+
+// Configurar uma Template Engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// Rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+// Ligar o servidor
+server.listen(5500)
